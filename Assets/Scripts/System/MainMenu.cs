c3e9349deeb1043a3df5231f28099c5eac04be13
Michael Jack <michaeljack255@gmail.com>
2019-06-22 18:53:27 +0900

    Added title

---
+++ Assets/Scripts/System/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void HowToPlay()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
