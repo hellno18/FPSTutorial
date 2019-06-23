444931dbf54fa3fd65a836f59dca4ac0b78df602
Michael Jack <michaeljack255@gmail.com>
2019-06-23 22:33:23 +0900

    Pause System

--- Assets/Scripts/System/TimeSystem.cs
+++ Assets/Scripts/System/TimeSystem.cs
@@ -2,33 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class TimeSystem : MonoBehaviour
 {
     public Text timeTextMinutes;
     public Text timeTextSeconds;
+    CanvasGroup pauseCanvas;
     [SerializeField]
     float timeLimit = 120f;
     float minutes;
     float seconds;
+    bool isPause;
     public enum GameStatus
     {
         Start,
         Finished,
+        Pause,
         Dead
     }
     public GameStatus gamestatus;
     // Start is called before the first frame update
     void Start()
     {
+        isPause = false;
+        pauseCanvas = GameObject.Find("Pause").GetComponent<CanvasGroup>();
+        pauseCanvas.alpha = 0;
         gamestatus = GameStatus.Start;
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimeCountDown();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPause)
+            {
+                pauseCanvas.alpha = 1;
+                Time.timeScale = 0;
+                gamestatus = GameStatus.Pause;
+                isPause = true;
+            }
+            else
+            {
+                pauseCanvas.alpha = 0;
+                Time.timeScale = 1;
+                isPause = false;
+                gamestatus = GameStatus.Start;
+            }
+            
+        }
+        switch (gamestatus)
+        {
+            case GameStatus.Start:
+                TimeCountDown();
+                break;
+
+            case GameStatus.Finished:
+                SceneManager.LoadScene("Result");
+                break;
+        }
+
+        
     }
 
+    //time countdown
     void TimeCountDown()
     {
         timeLimit -= Time.deltaTime;
