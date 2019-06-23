3ddb69c20787d152bbe5de185b1dac362c41663d
Michael Jack <michaeljack255@gmail.com>
2019-06-23 17:32:47 +0900

    Updated UI title

--- Assets/Scripts/System/MainMenu.cs
+++ Assets/Scripts/System/MainMenu.cs
@@ -6,10 +6,56 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     bool isJapanese;
+    CanvasGroup _canvasGroupButton;
+    GameObject _canvasGroupPanel;
+    [SerializeField]
+    private float speed = 0.2f;
+
+    private enum CanvasFader
+    {
+        None,
+        FadeIn,
+        Finished,
+        FadeOut
+    }
+    private CanvasFader _fadeState;
     // Start is called before the first frame update
     void Start()
     {
         isJapanese = false;
+        _fadeState = CanvasFader.None;
+        _canvasGroupButton = GameObject.Find("ButtonGroup").GetComponent<CanvasGroup>();
+        _canvasGroupPanel = GameObject.Find("Panel");
+        _canvasGroupPanel.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        switch (_fadeState)
+        {
+            case CanvasFader.FadeIn:
+                _canvasGroupButton.alpha -= speed;
+                if (_canvasGroupButton.alpha < 0)
+                {
+                    _canvasGroupButton.alpha = 0;
+                    _fadeState= CanvasFader.Finished;
+                }
+                _canvasGroupPanel.SetActive(true);
+                break;
+            case CanvasFader.Finished:
+                break;
+            case CanvasFader.FadeOut:
+                _canvasGroupButton.alpha += speed;
+                if(_canvasGroupButton.alpha > 1)
+                {
+                    _canvasGroupButton.alpha = 1;
+                    _fadeState = CanvasFader.None;
+                }
+                _canvasGroupPanel.SetActive(false);
+                break;
+
+        }
     }
 
     public void StartButton()
@@ -19,7 +65,7 @@ public class MainMenu : MonoBehaviour
 
     public void HowToPlay()
     {
-
+        _fadeState = CanvasFader.FadeIn;
     }
 
     public void Japanese()
@@ -35,6 +81,11 @@ public class MainMenu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void Back()
+    {
+        _fadeState = CanvasFader.FadeOut;
     }
 
     public bool GetBool()
--- Assets/Scripts/System/TranslateSystem.cs
+++ Assets/Scripts/System/TranslateSystem.cs
@@ -9,7 +9,10 @@ public class TranslateSystem : MonoBehaviour
     public Text startButton;
     public Text howToPlay;
     public Text exit;
+    public Text backButton;
+    public Text howtoPlayText;
     MainMenu system;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,12 +27,17 @@ public class TranslateSystem : MonoBehaviour
             startButton.text = "スタート";
             howToPlay.text = "遊び方";
             exit.text = "閉じる";
+            backButton.text = "戻る";
+            howtoPlayText.text = "ASDW  -  移動\n" + "R - リロード\n" + "CTRL - しゃがむ\n" + "Shift - 走る\n" + "左クリック　-　シュート\n";
+
         }
         else
         {
             startButton.text = "Start";
             howToPlay.text = "How To Play";
             exit.text = "Exit";
+            backButton.text = "Back";
+            howtoPlayText.text = "ASDW  -  Movement\n" + "R - Reload\n" + "CTRL - Crouch\n" + "Shift - Sprint\n" + "Left - Click - Shoot\n";
         }
         
     }
